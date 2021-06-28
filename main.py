6331a4a74414d1644f735c8205931924f550e43f
TakamiChie@Visnu <chie@onpu-tamago.net>
2021-06-28 11:02:56 +0900

    ドキュメントコメント追加

--- main.py
+++ main.py
@@ -10,18 +10,39 @@ from parsers.rss10 import RSS10Parser
 from parsers.rss20 import RSS20Parser
 
 class Entry:
+  """
+  エントリーを示す構造体
+  """
   def __init__(self, title: str, items: list[dict[str, str]]) -> None:
+    """
+    コンストラクタ
+
+    Parameters
+    ----
+    title: タイトル
+    items: エントリー内の項目一覧。title, link, descriptionが格納されている
+    """
     self.title = title
     self.items = items
 
 class Application:
   def start(self) -> None:
+    """
+    プログラムを開始する
+    """
     self.listboxes: list[tkinter.Listbox] = []
     self.entries = data = self.getlist()
     self.window = window = self.creategui(data)
     window.mainloop()
 
   def getlist(self) -> list[Entry]:
+    """
+    エントリーリストを取得する
+
+    Returns
+    ----
+    エントリーリスト
+    """
     result = []
     with open("config.yaml", "r", encoding="utf-8") as f:
       data = yaml.safe_load(f)
@@ -36,12 +57,35 @@ class Application:
     return result
 
   def creategui(self, entries: list[Entry]) -> tkinter.Tk:
+    """
+    GUIを構築する
+
+    Parameters
+    ----
+    entries: エントリーリスト
+
+    Returns
+    ----
+    ウィンドウオブジェクト
+    """
     window = tkinter.Tk("Tool")
     self.getbuttonbar(window).pack(expand=True, side=tkinter.BOTTOM, fill=tkinter.X)
     self.gettabs(window, entries).pack(expand=True, fill=tkinter.BOTH)
     return window
 
   def gettabs(self, owner: tkinter.Tk, entries: list[Entry]) -> tkinter.ttk.Notebook:
+    """
+    タブオブジェクトを生成する
+
+    Parameters
+    ----
+    owner: オーナーとなるウィンドウオブジェクト
+    entries: エントリーリスト
+
+    Returns
+    ----
+    タブオブジェクト
+    """
     note = tkinter.ttk.Notebook(owner)
     for entry in entries:
       content = tkinter.Listbox(note)
@@ -54,6 +98,17 @@ class Application:
     return note
 
   def getbuttonbar(self, owner: tkinter.Frame) -> tkinter.Frame:
+    """
+    ボタンバーを生成する
+
+    Parameters
+    ----
+    owner: オーナーとなるウィンドウオブジェクト
+
+    Returns
+    ----
+    ボタンバー
+    """
     frame = tkinter.Frame(owner)
     openbtn = tkinter.Button(frame, text="OPEN", command=self.on_openbtn_click)
     openbtn.pack(fill=tkinter.X, side=tkinter.LEFT, expand=True, padx=4, pady=4)
@@ -65,11 +120,21 @@ class Application:
     return frame
 
   def on_openbtn_click(self) -> None:
+    """
+    OPENボタンクリック時の動作
+    """
     item = self.getcurrentitem()
     if item is not None:
       webbrowser.open(item["link"])
 
   def on_copybtn_click(self, selection: str) -> None:
+    """
+    COPYボタンクリック時の動作
+
+    Parameters
+    ----
+    selection: 選択したメニュー項目のキャプション
+    """
     item = self.getcurrentitem()
     if item is not None:
       text = ""
@@ -86,6 +151,13 @@ class Application:
 
 
   def getcurrentitem(self) -> Optional[dict[str, str]]:
+    """
+    現在の選択中タブのリストで選択中の項目を示す値を取得する
+
+    Returns
+    ----
+    値。項目が選択されていない場合はNone
+    """
     index = self.window.children["!notebook"].index(self.window.children["!notebook"].select())
     return self.entries[index].items[self.listboxes[index].curselection()[0]] \
       if len(self.listboxes[index].curselection()) == 1 else None
